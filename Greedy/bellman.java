84dc60087fa5936d54adcff81fd6e67b1addd845
Gautham Vanjre <vanjregautham@gmail.com>
2021-09-17 20:08:37 +0530

    added bellman ford algorithm

---
+++ Greedy/bellman.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class bellman{
+	static void Bellman(int graph[][],int v,int e,int src){
+		int[] dist=new int[v];
+
+		for(int i=0;i<v;i++){
+			dist[i]=Integer.MAX_VALUE;	//initialize distances of all vertices as infinite
+		}
+
+		// initialize distance of source as 0
+		dist[src]=0;
+
+		// relax till v-1 times
+		for(int i=0;i<v-1;i++){
+			for(int j=0;j<e;j++){
+				if(dist[graph[j][0]] != Integer.MAX_VALUE && dist[graph[j][0]] +graph[j][2]<dist[graph[j][1]])
+					dist[graph[j][1]]=dist[graph[j][0] +graph[j][2]];
+			}//j loop
+		}//i loop
+
+
+		// this checks for negative weight cycles
+		for (int i = 0; i < e; i++)
+    	{
+        	int x = graph[i][0];
+        	int y = graph[i][1];
+        	int weight = graph[i][2];
+       	 	if (dist[x] != Integer.MAX_VALUE &&
+                dist[x] + weight < dist[y])
+            System.out.println("Graph contains negative"
+                    +" weight cycle");
+    	}
+ 
+    	System.out.println("Vertex Distance from Source");
+    	for (int i = 0; i < v; i++)
+        	System.out.println(i + "\t\t" + dist[i]);
+		
+}//Bellman function 
+
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number of vertices");
+	int v=sc.nextInt();
+
+	System.out.println("Enter the number of edges");
+	int e=sc.nextInt();
+
+	int graph[][] = { { 0, 1, -1 }, { 0, 2, 4 },
+                    { 1, 2, 3 }, { 1, 3, 2 },
+                    { 1, 4, 2 }, { 3, 2, 5 },
+                    { 3, 1, 1 }, { 4, 3, -3 } };
+
+	Bellman(graph,v,e,0);
+}//main method	
+
+}//class 
